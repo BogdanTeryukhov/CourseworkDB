1587d5a785d5bbf30b1617a74752d4879279eda3
BogdanTeryukhov <94743986+BogdanTeryukhov@users.noreply.github.com>
2024-05-20 21:20:03 +0300

    Delete src directory

--- src/main/java/com/example/courseworkdatabases/exception/CantChangeIdException.java
+++
@@ -1,9 +0,0 @@
-package com.example.courseworkdatabases.exception;
-
-import java.io.IOException;
-
-public class CantChangeIdException extends IOException {
-    public CantChangeIdException(String message) {
-        super(message);
-    }
-}
